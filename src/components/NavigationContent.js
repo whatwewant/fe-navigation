f19fc8835028886897b7510068a6b7807363ea71
whatwewant <tobewhatwewant@gmail.com>
2017-06-15 09:31:07 +0800

    :bug:(navContent: scroll) relative + absolute(width+height:100% + scroll)

--- src/components/NavigationContent.js
+++ src/components/NavigationContent.js
@@ -3,21 +3,21 @@
 * @Date:   2017-06-14T18:12:43+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-06-14T23:30:08+08:00
+* @Last modified time: 2017-06-15T09:29:40+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
 
 import React from 'react';
 
-import { View, Text, List, ListItem } from 'elfen';
+import { View, Absolute, Text, List, ListItem } from 'elfen';
 
 import Card from './Card';
 
 export default function Navigation(props) {
   const { name, list = [] } = props;
   return (
-    <View>
+    <View position="relative" width="100%" height="100%">
       <Text
         size={24}
         color={'#f0506e'}
@@ -30,18 +30,20 @@ export default function Navigation(props) {
       >
         {name}
       </Text>
-      <List style={{ padding: 30, display: 'flex', flexWrap: 'wrap' }}>
-        {list.map((item, key) => (
-          <ListItem key={key}>
-            <Card
-              logo={item.logo}
-              name={item.name}
-              description={item.description}
-              homepage={item.homepage}
-            />
-          </ListItem>
-        ))}
-      </List>
+      <Absolute top={107} right={0} bottom={0} left={0}>
+        <List width="100%" height="100%" style={{ padding: 30, display: 'flex', flexWrap: 'wrap' }}>
+          {list.map((item, key) => (
+            <ListItem key={key}>
+              <Card
+                logo={item.logo}
+                name={item.name}
+                description={item.description}
+                homepage={item.homepage}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Absolute>
     </View>
   );
 }
