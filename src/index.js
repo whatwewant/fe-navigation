dd49e4adb768de85b5495a1c3a56bbe9d08315c1
tobewhatwewant <tobewhatwewant@gmail.com>
2017-06-16 01:02:31 +0800

    :sparkles:(PWA)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@
 * @Author: eason
 * @Date:   2017-01-04T17:00:03+08:00
 * @Email:  uniquecolesmith@gmail.com
-* @Last modified by:   eason
-* @Last modified time: 2017-06-14T22:54:27+08:00
+ * @Last modified by:   eason
+ * @Last modified time: 2017-06-16T01:02:12+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
 
 import dva from 'dva';
+import OfflinePluginRuntime from 'offline-plugin/runtime';
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') {
+  OfflinePluginRuntime.install();
+}
 
 // 1. Initialize
 const app = dva();
