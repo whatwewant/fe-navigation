87534601fe1142bee7b866c5279a720c00e3933f
tobewhatwewant <tobewhatwewant@gmail.com>
2017-06-18 03:46:21 +0800

    :heavy_plus_add:(respinner)

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,7 +3,7 @@
 * @Date:   2016-12-15T13:48:42+08:00
 * @Email:  uniquecolesmith@gmail.com
  * @Last modified by:   eason
- * @Last modified time: 2017-06-16T21:47:42+08:00
+ * @Last modified time: 2017-06-18T03:46:18+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
@@ -12,6 +12,8 @@ import React from 'react';
 import { connect } from 'dva';
 import { View, Flex } from 'elfen';
 import Helmet from 'react-helmet';
+import Beat from 'respinner/lib/beat';
+
 import styles from './App.css';
 
 import Navigation from '../components/Navigation';
@@ -26,10 +28,15 @@ function IndexPage(props) {
         <link rel="shortcut icon" href={logo} />
       </Helmet>
       <View width={280} style={{ boxShadow: '0 5px 15px rgba(0,0,0,.08)' }}>
-        <Navigation navigations={props.navs} onSelectNav={props.onSelectNav} />
+        {props.navs.length === 0
+          ? <Beat style={{ marginTop: 100 }} fill="#4197ff" count={4} />
+          : <Navigation navigations={props.navs} onSelectNav={props.onSelectNav} />
+        }
       </View>
       <View flex={1}>
-        <NavigationContent name={props.name} list={props.list} />
+        {props.list.length
+          ? <NavigationContent name={props.name} list={props.list} />
+        : null}
       </View>
     </Flex>
   );
